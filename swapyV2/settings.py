18095da7c792c68d9eabbf465064dd7f682f412b
Pablo Arellano <pablo.arellanos97@gmail.com>
2019-01-26 20:54:15 -0800

    12

--- swapyV2/settings.py
+++ swapyV2/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import dj_database_url
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -138,4 +137,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 
-django_heroku.settings(locals())
