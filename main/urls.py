132a087d3f1b99c6c78f82c79605ce086481a1e4
Pablo Arellano <pablo.arellanos97@gmail.com>
2019-01-26 19:42:57 -0800

    1.0

---
+++ main/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import login, register
+
+urlpatterns = [
+    path('login', login),
+    path('register', register)
+]
